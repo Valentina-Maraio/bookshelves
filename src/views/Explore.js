6651ba2f786db2c6c0f78d8a246005d9eba77eba
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-03 11:15:37 +0100

    list

--- src/views/Explore.js
+++ src/views/Explore.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../App.css";
 import MenuButton from "../button/MenuButton";
+import books from '../data/books.json'
+import './style/Explore.css'
 
 const Explore = () => {
   return (
@@ -11,6 +13,16 @@ const Explore = () => {
       <div className="App">
         <header className="App-explore">
           <h3>Explore</h3>
+          <div>
+            {books?.books?.map((item) => {
+              return(
+                <div className="card_box" key={item.id}>
+                  <h4>{item.title}</h4>
+                  <h5>{item.author}</h5>
+                </div>
+              )
+            })}
+          </div>
         </header>
       </div>
     </>

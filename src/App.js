6212e3b377a3a717855a71e4679199fab73cacf5
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-03 19:21:00 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Profile from './views/Profile'
-import Explore from './views/Explore'
-import SignIn from './registration/SignIn'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Profile from "./views/Profile";
+import Explore from "./views/Explore";
+import SignIn from "./registration/SignIn";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
-        <Route path="/explore" element={<Explore/>}></Route>
-        <Route path="/signin" element={<SignIn/>}></Route>
-      </Routes>
-    </Router>
+    <>
+      <NextUIProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/explore" element={<Explore />}></Route>
+            <Route path="/signin" element={<SignIn />}></Route>
+          </Routes>
+        </Router>
+      </NextUIProvider>
+    </>
   );
 }
 
--- src/views/Explore.js
+++ src/views/Explore.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../App.css";
 import MenuButton from "../button/MenuButton";
-import books from '../data/books.json'
-import './style/Explore.css'
+import books from "../data/books.json";
+import "./style/Explore.css";
+import { Card, Button, Text, Grid, Row, Col } from "@nextui-org/react";
 
 const Explore = () => {
   return (
@@ -13,16 +14,87 @@ const Explore = () => {
       <div className="App">
         <header className="App-explore">
           <h3>Explore</h3>
-          <div>
+          <Grid.Container gap={2} justify="flex-start">
             {books?.books?.map((item) => {
-              return(
-                <div className="card_box" key={item.id}>
-                  <h4>{item.title}</h4>
-                  <h5>{item.author}</h5>
-                </div>
-              )
+              return (
+                <>
+                  <Grid xs={6} sm={3} key={item.id}>
+                    <Card isPressable>
+                      <Card.Header
+                        css={{ position: "absolute", zIndex: 1, top: 5 }}
+                      >
+                        <Col>
+                          <Button
+                            flat
+                            auto
+                            rounded
+                            css={{ color: "red", bg: "#94f9f026" }}
+                          >
+                            <Text
+                              css={{ color: "inherit" }}
+                              size={12}
+                              weight="bold"
+                              transform="uppercase"
+                            >
+                              Fav
+                            </Text>
+                          </Button>
+                        </Col>
+                      </Card.Header>
+                      <Card.Body css={{ p: 0 }}>
+                        <Card.Image
+                          objectFit="cover"
+                          width="100%"
+                          height={140}
+                        />
+                      </Card.Body>
+                      <Card.Footer
+                        isBlurred
+                        css={{
+                          position: "absolute",
+                          bgBlur: "#0f111466",
+                          borderTop: "$borderWeights$light solid $gray800",
+                          bottom: 0,
+                          zIndex: 1,
+                        }}
+                      >
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>
+                                <Text color="#d1d1d1" size={12}>
+                                  {item.author}
+                                </Text>
+                              </Col>
+                            </Row>
+                          </Col>
+                          <Col>
+                            <Row justify="flex-end">
+                              <Button
+                                flat
+                                auto
+                                rounded
+                                css={{ color: "#94f9f0", bg: "#94f9f026" }}
+                              >
+                                <Text
+                                  css={{ color: "inherit" }}
+                                  size={12}
+                                  weight="bold"
+                                  transform="uppercase"
+                                >
+                                  Info
+                                </Text>
+                              </Button>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Card.Footer>
+                    </Card>
+                  </Grid>
+                </>
+              );
             })}
-          </div>
+          </Grid.Container>
         </header>
       </div>
     </>
